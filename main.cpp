aaf1a66a49a24157a7356931a255ef67463c6a06
Большагин Вячеслав <gbublik@gmail.com>
2025-06-08 09:56:50 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "httplib.h"
+#include <fstream>
+#include <string>
+
+std::string readFile(const char* path) {
+    if (std::ifstream file(path); file.is_open()) {
+        std::ostringstream ss;
+        ss << file.rdbuf();
+        file.close();
+        return ss.str();
+    } else {
+        std::cerr << "Unable to open file" << std::endl;
+        exit(1);
+    }
+}
+
+int main() {
+    // HTTPS
+    httplib::Server svr;
+
+    svr.Get("/", [](const httplib::Request &, httplib::Response &res) {
+      res.set_content(readFile("../AuthorizationRequest.html"), "text/html; charset=UTF-8");
+    });
+
+    svr.listen("127.0.0.1", 8080);
+    return 0;
+}
